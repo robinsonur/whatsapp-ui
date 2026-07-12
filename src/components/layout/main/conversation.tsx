7f53414b5044f14d7a86062b68efa1542e6c1be1
Robinson Ureña Rodríguez <robinsonurodriguez@gmail.com>
2026-07-12 05:03:22 -0400

    FEAT: Add Conversation component for chat interface display.
    - Introduced a new `Conversation` component to present chat details including title, summary, time, and an optional amount.
    - Integrated `Counter` and `IconButton` components for enhanced user interaction and visual appeal.
    - Updated the UI index to export the new `Counter` component for broader accessibility.

---
+++ src/components/layout/main/conversation.tsx
@@ -0,0 +1,43 @@
+import { IconButton, Counter } from "@/components/ui";
+import { cn } from "@/lib/utils";
+import { UsersIcon } from "lucide-react";
+import { Activity } from "react";
+
+type ConversationProps = {
+  title: string;
+  summary: string;
+  time: string;
+  amount?: number;
+};
+
+function Conversation({ title, summary, time, amount }: ConversationProps) {
+  return (
+    <article className="grid grid-cols-[auto_1fr_auto] grid-rows-3 items-center gap-x-4 gap-y-1">
+      <IconButton className="col-start-1 row-span-3 row-start-2 border-0 bg-amber-700">
+        <UsersIcon />
+      </IconButton>
+      <span className="col-start-2 col-end-4 row-start-1 border-white/10 border-t" />
+      <strong className="col-start-2 row-start-2 min-w-0 truncate">
+        {title}
+      </strong>
+      <span
+        className={cn(
+          "col-start-3 row-start-2 justify-self-end text-white/50",
+          amount && "text-primary",
+        )}
+      >
+        {time}
+      </span>
+      <p className="col-start-2 row-start-3 min-w-0 truncate text-white/50">
+        {summary}
+      </p>
+      <Activity mode={amount ? "visible" : "hidden"}>
+        <Counter className="col-start-3 row-start-3 justify-self-end">
+          {amount}
+        </Counter>
+      </Activity>
+    </article>
+  );
+}
+
+export { Conversation, type ConversationProps };
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,9 @@
 import { Container, type ContainerProps } from "./container";
+import { Counter, type CounterProps } from "./counter";
 import { IconButton, type IconButtonProps } from "./icon-button";
 
-export { Container, type ContainerProps, IconButton, type IconButtonProps };
+export {
+  Container, type ContainerProps,
+  Counter, type CounterProps,
+  IconButton, type IconButtonProps,
+};
