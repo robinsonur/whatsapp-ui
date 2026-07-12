d330967c0aea4adea4c00a35d6c8d22195abfbcc
Robinson Ureña Rodríguez <robinsonurodriguez@gmail.com>
2026-07-12 01:33:38 -0400

    FEAT: Create index file for UI components export.
    - Added an `index.ts` file to streamline the export of components along with their respective types.
    - This facilitates easier imports and enhances the organization of UI components.

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+import { Container, type ContainerProps } from "./container";
+import { IconButton, type IconButtonProps } from "./icon-button";
+
+export {
+  Container, type ContainerProps,
+  IconButton, type IconButtonProps,
+};
